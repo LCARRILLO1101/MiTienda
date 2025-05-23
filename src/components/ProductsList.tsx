31525e186e9022f6dbb888c4f7f1b2b389d2885c
LCARRILLO1101 <liamcr505@gmail.com>
2025-05-23 13:10:45 -0600

    agregue key a ProductsCard

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -7,7 +7,7 @@ const ProductsList = ({products}:any) => {
         <h1 className="text-3xl font-bold text-center mb-6">Mi lista de Productos</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {products.map((product:Product)=>(
-                <ProductsCard product={product}/>
+                <ProductsCard key={product.id} product={product}/>
             ))}
         </div>
     </div>
