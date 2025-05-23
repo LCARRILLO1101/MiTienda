ea77a089513c55aedb5fb7bfd239c0481891fecc
LCARRILLO1101 <liamcr505@gmail.com>
2025-05-23 12:30:28 -0600

    Contenido App.tsx, creacion ProductsCard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
+import { useState, useEffect } from 'react'
 import './App.css'
-import Products from './components/Products'
+import axios from 'axios'
+import ProductsList from './components/ProductsList';
+import { MdErrorOutline } from "react-icons/md";
+
+export interface Rating{
+  rate:number;
+  count:number;
+}
+
+export interface Product{
+  id:number;
+  image:string;
+  title:string;
+  category:string;
+  description:string;
+  price:number;
+  rating:Rating;
+
+}
+
 
 function App() {
+  const [products,setProducts] = useState<Product[]>([]);
+  const [loading,setLoading] = useState<boolean>(true);
+  const [error,setError] = useState<string|null>(null);
+
+  useEffect(()=>{
+    const fetchProducts = async():Promise<void>=>{
+      try{
+        const res = await axios.get<Product[]>(`https://fakestoreapi.com/products`);
+        setProducts(res.data);
+        setLoading(false);
+      }catch (error){
+        if(error instanceof Error){
+          setLoading(false);
+          setError(error.message);
+        }else{
+          setLoading(false);
+          setError(`Hubo un error en la cunsulta`);
+        }
+        console.log(`Error al consultar los productos`)
+      }
+    }
+
+    fetchProducts();
+
+  },[]);
+
+  console.log(products)
+
+  if(loading){
+    return (<div className='flex flex-cold justify-center'>
+      <h2>Loading...</h2>
+      <div className="loader"></div>
+    </div>)
+  }
+
+  if(error){
+    return <p><span className='text-red-500'><MdErrorOutline /></span>Hubo un error...<span className='text-red-500'><MdErrorOutline /></span></p>
+  }
 
   return (
     <>
-      <Products/>
+      <ProductsList/>
     </>
   )
 }
---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,9 @@
+import type { Product } from "../App"
+
+const ProductsList = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default ProductsList
